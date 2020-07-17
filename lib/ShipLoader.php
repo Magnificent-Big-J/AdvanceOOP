fe08cdadad8aa29719d7f8137d86241362dbd79d
Joel Mnisi <joel.mnisi@protonmail.com>
2020-07-17 22:28:27 +0200

    Centralizing configuration

---
+++ lib/ShipLoader.php
@@ -0,0 +1,82 @@
+<?php
+
+
+class ShipLoader
+{
+    private $pdo;
+    /**
+     * @return Ship[]
+     */
+    public function getShips()
+    {
+        $shipsData = $this->queryForShips();
+        $ships = array();
+
+        foreach ($shipsData as $shipData) {
+
+            $ships[] = $this->createShipFromData($shipData);
+        }
+
+        return $ships;
+    }
+
+    /**
+     * @param $id
+     * @return Ship|null
+     */
+    public function findOneById($id)
+    {
+        $pdo = $this->getPDO();
+        $statement = $pdo->prepare('SELECT * FROM ship where id = :id');
+        $statement->execute(array('id'=> $id));
+        $shipArray = $statement->fetch(PDO::FETCH_ASSOC);
+
+        if (!$shipArray) {
+            return null;
+        }
+
+        return $this->createShipFromData($shipArray);
+    }
+
+    /**
+     * @param array $shipData
+     * @return Ship
+     * @throws Exception
+     */
+    private function createShipFromData(array  $shipData)
+    {
+        $ship = new Ship($shipData['name']);
+        $ship->setId($shipData['id']);
+        $ship->setWeaponPower($shipData['weapon_power']);
+        $ship->setJediFactor($shipData['jedi_factor']);
+        $ship->setStrength($shipData['strength']);
+
+        return $ship;
+    }
+
+    /**
+     * @return array
+     */
+    private function queryForShips()
+    {
+        $pdo = $this->getPDO();
+        $statement = $pdo->prepare('SELECT * FROM ship');
+        $statement->execute();
+
+        return $statement->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    /**
+     * @return PDO
+     */
+    private function getPDO(){
+        if ($this->pdo === null) {
+            $pdo = new PDO('mysql:host=localhost;dbname=oo_battle','devOps', 'devOps64');
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->pdo = $pdo;
+        }
+
+        return $this->pdo;
+    }
+
+}
