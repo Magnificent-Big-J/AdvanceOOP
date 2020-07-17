8b9eac0cbeacfda0586f9f9a4af2012b220a72eb
Joel Mnisi <joel.mnisi@protonmail.com>
2020-07-17 23:21:31 +0200

    Singleton

--- battle.php
+++ battle.php
@@ -2,8 +2,7 @@
 require __DIR__ . '/bootstrap.php';
 
 $container = new Container($configuration);
-$pdo = $container->getPDO();
-$shipLoader = new ShipLoader($pdo);
+$shipLoader = $container->getShipLoader();
 $ships = $shipLoader->getShips();
 
 $ship1Id = isset($_POST['ship1_id']) ? $_POST['ship1_id'] : null;
@@ -29,8 +28,8 @@ if ($ship1Quantity <= 0 || $ship2Quantity <= 0) {
     die;
 }
 
-$battleManager = new BattleManager();
-$battleResult = $battleManager->battle($ship1, $ship1Quantity, $ship2, $ship2Quantity);
+
+$battleResult = $container->getBattleManager()->battle($ship1, $ship1Quantity, $ship2, $ship2Quantity);
 
 ?>
 
--- lib/Container.php
+++ lib/Container.php
@@ -4,7 +4,14 @@
 class Container
 {
     private $configuration;
+    private $pdo;
+    private $shipLoader;
+    private $battleManager;
 
+    /**
+     * Container constructor.
+     * @param $configuration
+     */
     public function __construct($configuration)
     {
         $this->configuration = $configuration;
@@ -15,11 +22,36 @@ class Container
      */
     public function getPDO()
     {
-        $pdo = new PDO(
-            $this->configuration['db_dsn'],
-            $this->configuration['db_user'],
-            $this->configuration['db_pass']);
+        if ($this->pdo === null ) {
+            $this->pdo = new PDO(
+                $this->configuration['db_dsn'],
+                $this->configuration['db_user'],
+                $this->configuration['db_pass']);
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
 
-        return $pdo;
+
+        return $this->pdo;
+    }
+
+    /**
+     * @return ShipLoader
+     */
+    public function getShipLoader()
+    {
+        if($this->shipLoader === null) {
+            $this->shipLoader = new ShipLoader($this->getPDO());
+        }
+
+        return $this->shipLoader;
+    }
+
+    public function getBattleManager()
+    {
+        if($this->battleManager === null) {
+            $this->battleManager = new BattleManager();
+        }
+
+        return $this->battleManager;
     }
 }
